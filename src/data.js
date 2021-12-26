4a99e73fc32c19da87e24a55661cdcdc736ba0bf
maxkram <max.kramer@live.com>
2021-12-25 19:51:43 -0500

    first and last one

---
+++ src/data.js
@@ -0,0 +1,66 @@
+const menu = [
+  {
+    id: 1,
+    title: 'String art',
+    category: 'Arts',
+    img: './images/string.jpg',
+    desc: `An Iraqi artist who used string to create a portrait that measures more than 67 square feet achieved a Guinness World Record for the largest pin and thread art.`,
+  },
+  {
+    id: 2,
+    title: 'Brotherhood',
+    category: 'People are metal',
+    img: './images/brother.jpg',
+    desc: `Brothers reclaim record for climbing stairs with one balanced on other's head`,
+  },
+  {
+    id: 3,
+    title: 'Gems in mountain',
+    category: 'People are metal',
+    img: './images/gems.jpg',
+    desc: `A municipality in France said a climber who found $168,000 worth of precious gems on a mountain will be allowed to keep half of the find after the original owners couldn't be located.`,
+  },
+  {
+    id: 4,
+    title: 'Flying squirrels',
+    category: 'Awws',
+    img: './images/squirrel.jpg',
+    desc: `A crew working to cut down a dying oak tree on the University of Nebraska's East Campus made an unexpected discovery in a hollow limb -- a family of flying squirrels.`,
+  },
+  {
+    id: 5,
+    title: 'Tv taste',
+    category: 'Science, b***!',
+    img: './images/tv.jpg',
+    desc: `A professor at a Japanese university unveiled his latest innovation: a TV with a screen the viewer can taste.`,
+  },
+  {
+    id: 6,
+    title: "Rubik's record",
+    category: 'No way!',
+    img: './images/rubik.jpg',
+    desc: `A California teenager with a talent for solving Rubik's cubes unofficially broke two world records by solving a cube while juggling and solving a cube while hanging upside down.`,
+  },
+  {
+    id: 7,
+    title: 'Leopard kitten saved',
+    category: 'Awws',
+    img: './images/leopard.jpg',
+    desc: `Wildlife officials in India said a man who rescued what he thought were a pair of abandoned kittens on his property brought them inside his home and only later discovered they were leopard cubs.`,
+  },
+  {
+    id: 8,
+    title: '15-year-old graduate',
+    category: 'People are metal',
+    img: './images/15.jpg',
+    desc: `A 15-year-old boy is set to become the youngest graduate in the history of the University of Nevada, Las Vegas, when he is presented with his fifth degree next week.`,
+  },
+  {
+    id: 9,
+    title: 'Camel beauty contest',
+    category: 'People are crazy',
+    img: './images/camel.jpg',
+    desc: `Organizers of a beauty contest for camels in Saudi Arabia said 43 animals have been disqualified for receiving Botox treatments, face lifts and other cosmetic procedures.`,
+  },
+];
+export default menu;
